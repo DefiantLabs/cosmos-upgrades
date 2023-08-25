9379daab8d35c67fc741b2307ecb864615b5e7d1
danb <dan@bryanlabs.net>
2023-08-25 08:04:13 -0400

    works

--- cosmos-upgrades.py
+++ cosmos-upgrades.py
@@ -6,6 +6,7 @@ from random import shuffle
 import json
 import logging
 from flask import Flask, jsonify, request
+from concurrent.futures import ThreadPoolExecutor
 
 
 app = Flask(__name__)
@@ -18,9 +19,7 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 BASE_URL = "https://raw.githubusercontent.com/cosmos/chain-registry/master"
 
-# MAINNETS = ["osmosis", "neutron", "nolus", "crescent", "akash", "cosmoshub", "sentinel", "stargaze", "omniflixhub", "terra", "kujira", "stride", "injective", "juno"]
-MAINNETS = ["akash"]
-TESTNETS = ["cosmoshubtestnet"]  # You'll need to fill in the testnets if they're not dynamic
+
 MAINNET_BASE_URL = "https://raw.githubusercontent.com/cosmos/chain-registry/master"
 TESTNET_BASE_URL = "https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets"
 
@@ -28,6 +27,38 @@ SEMANTIC_VERSION_PATTERN = re.compile(r'v(\d+(?:\.\d+){0,2})')
 
 # these servers have given consistent error responses, this list is used to skip them
 SERVER_BLACKLIST = ["https://stride.api.bccnodes.com:443", "https://api.omniflix.nodestake.top", "https://cosmos-lcd.quickapi.com:443"]
+
+def get_healthy_rpc_endpoints(rpc_endpoints):
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        healthy_rpc_endpoints = [rpc for rpc, is_healthy in executor.map(lambda rpc: (rpc, is_endpoint_healthy(rpc['address'])), rpc_endpoints) if is_healthy]
+    
+    return healthy_rpc_endpoints[:5]  # Select the first 5 healthy RPC endpoints
+
+def get_healthy_rest_endpoints(rest_endpoints):
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        healthy_rest_endpoints = [rest for rest, is_healthy in executor.map(lambda rest: (rest, is_endpoint_healthy(rest['address'])), rest_endpoints) if is_healthy]
+    
+    return healthy_rest_endpoints[:5]  # Select the first 5 healthy REST endpoints
+
+def is_endpoint_healthy(endpoint):
+    try:
+        response = requests.get(f"{endpoint}/health", timeout=3, verify=False)
+        print(response.status_code)
+        return response.status_code == 200
+    except:
+        return False
+
+def get_healthy_endpoints(endpoints):
+    healthy_endpoints = []
+    
+    def check_endpoint(endpoint):
+        if is_endpoint_healthy(endpoint['address']):
+            healthy_endpoints.append(endpoint)
+    
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(check_endpoint, endpoints)
+    
+    return healthy_endpoints
 
 def check_rest_endpoint(rest_url):
     """Check the REST endpoint and return the application version and response time."""
@@ -53,9 +84,9 @@ def get_latest_block_height_rpc(rpc_url):
     except requests.RequestException as e:
         return -1  # Return -1 to indicate an error
 
-def fetch_all_endpoints(network_type, base_url):
+def fetch_all_endpoints(network_type, base_url, request_data):
     """Fetch all the REST and RPC endpoints for all networks and store in a map."""
-    networks = MAINNETS if network_type == "mainnet" else TESTNETS
+    networks = request_data.get("MAINNETS", []) if network_type == "mainnet" else request_data.get("TESTNETS", [])
     endpoints_map = {}
     for network in networks:
         rest_endpoints, rpc_endpoints = fetch_endpoints(network, base_url)
@@ -69,6 +100,7 @@ def fetch_endpoints(network, base_url):
     """Fetch the REST and RPC endpoints for a given network."""
     try:
         response = requests.get(f"{base_url}/{network}/chain.json")
+        print(f"{base_url}/{network}/chain.json")
         response.raise_for_status()
         data = response.json()
         rest_endpoints = data.get("apis", {}).get("rest", [])
@@ -150,16 +182,24 @@ def fetch_current_upgrade_plan(rest_url):
         raise e
 
 def fetch_data_for_network(network, endpoints, network_type):
+    
     print(f"Fetching data for network {network}")
     rest_endpoints = endpoints.get("rest", [])
     rpc_endpoints = endpoints.get("rpc", [])
+    print(f"Found {len(rest_endpoints)} rest endpoints and {len(rpc_endpoints)} rpc endpoints")
     
     # Prioritize RPC endpoints for fetching the latest block height
     # Shuffle RPC endpoints to avoid calling the same one over and over
     latest_block_height = -1
-    shuffle(rpc_endpoints)
+    healthy_rpc_endpoints = get_healthy_rpc_endpoints(rpc_endpoints)
+    healthy_rest_endpoints = get_healthy_rest_endpoints(rest_endpoints)
+    
+    # Shuffle the healthy endpoints
+    shuffle(healthy_rpc_endpoints)
+    shuffle(healthy_rest_endpoints)
+
     rpc_server_used = ""
-    for rpc_endpoint in rpc_endpoints:
+    for rpc_endpoint in healthy_rpc_endpoints:
         latest_block_height = get_latest_block_height_rpc(rpc_endpoint['address'])
         if latest_block_height > 0:
             rpc_server_used = rpc_endpoint['address']
@@ -167,12 +207,12 @@ def fetch_data_for_network(network, endpoints, network_type):
     
     # Check for active upgrade proposals
     # Shuffle RPC endpoints to avoid calling the same one over and over
-    shuffle(rest_endpoints)
+    shuffle(healthy_rest_endpoints)
     upgrade_block_height = None
     upgrade_version = ""
     source = ""
     
-    for index, rest_endpoint in enumerate(rest_endpoints):
+    for index, rest_endpoint in enumerate(healthy_rest_endpoints):
         current_endpoint = rest_endpoint["address"]
         
         if current_endpoint in SERVER_BLACKLIST:
@@ -181,7 +221,7 @@ def fetch_data_for_network(network, endpoints, network_type):
             active_upgrade_version, active_upgrade_height = fetch_active_upgrade_proposals(current_endpoint)
             current_upgrade_version, current_upgrade_height = fetch_current_upgrade_plan(current_endpoint)
         except:
-            if index + 1 < len(rest_endpoints):
+            if index + 1 < len(healthy_rest_endpoints):
                 print(f"Failed to query rest endpoints {current_endpoint}, trying next rest endpoint")
                 continue
             else:
@@ -212,26 +252,32 @@ def fetch_data_for_network(network, endpoints, network_type):
     }
     return output_data
 
-@app.route('/fetch', methods=['POST'])  # Change method to POST
+@app.route('/fetch', methods=['POST'])
 def fetch_network_data():
     try:
-        request_data = request.get_json()  # Get the JSON payload from the user
+        request_data = request.get_json()
         if not request_data:
             return jsonify({"error": "Invalid payload"}), 400
         
         results = []
-        for network_type, base_url, networks in [("mainnet", MAINNET_BASE_URL, request_data.get("MAINNETs", [])), 
-                                                 ("testnet", TESTNET_BASE_URL, request_data.get("TESTNETs", []))]:
-            endpoints_map = fetch_all_endpoints(network_type, base_url)
+        for network_type, base_url, networks in [("mainnet", BASE_URL, request_data.get("MAINNETS", [])), 
+                                                 ("testnet", BASE_URL + "/testnets", request_data.get("TESTNETS", []))]:
+            endpoints_map = fetch_all_endpoints(network_type, base_url, request_data)
             for network in networks:
                 try:
                     network_data = fetch_data_for_network(network, endpoints_map.get(network, {}), network_type)
                     results.append(network_data)
                 except Exception as e:
                     logging.error(f"Error fetching data for network {network}: {e}")
-        return jsonify(results)
+        
+        # Sort the results by 'upgrade_found' in descending order (chain upgrades first)
+        sorted_results = sorted(results, key=lambda x: x['upgrade_found'], reverse=True)
+        
+        return jsonify(sorted_results)
+    
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
