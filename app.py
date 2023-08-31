8579e73d3b663f6975b6f1ac2ed0e18f37791493
clemensgg <csc@soundportal.at>
2023-08-31 14:34:19 +0200

    refactor: repo download & periodic caching

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import requests
-from urllib3.exceptions import InsecureRequestWarning
 import re
 from datetime import datetime
 from random import shuffle
-import json
 import logging
+import threading
 from flask import Flask, jsonify, request
+from flask_caching import Cache
 from concurrent.futures import ThreadPoolExecutor
 from time import sleep
-import threading
-
-
+import os
+import zipfile
+import json
 
 app = Flask(__name__)
 
@@ -18,22 +18,66 @@ app = Flask(__name__)
 logging.basicConfig(filename='app.log', level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Suppress only the single InsecureRequestWarning from urllib3
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
-BASE_URL = "https://raw.githubusercontent.com/cosmos/chain-registry/master"
+# Initialize cache
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+# Initialize repo path var
+repo_path = ""
+
+# Initialize number of workers
+num_workers = int(os.environ.get('NUM_WORKERS', 10))
+
 GITHUB_API_BASE_URL = "https://api.github.com/repos/cosmos/chain-registry/contents"
 
-MAINNET_BASE_URL = "https://raw.githubusercontent.com/cosmos/chain-registry/master"
-TESTNET_BASE_URL = "https://raw.githubusercontent.com/cosmos/chain-registry/master/testnets"
-
-SEMANTIC_VERSION_PATTERN = re.compile(r'v(\d+(?:\.\d+){0,2})')
+# these servers have given consistent error responses, this list is used to skip them
+SERVER_BLACKLIST = ["https://stride.api.bccnodes.com:443", "https://api.omniflix.nodestake.top", "https://cosmos-lcd.quickapi.com:443"]
 
 # Global variables to store the data for mainnets and testnets
 MAINNET_DATA = []
 TESTNET_DATA = []
 
-# these servers have given consistent error responses, this list is used to skip them
-SERVER_BLACKLIST = ["https://stride.api.bccnodes.com:443", "https://api.omniflix.nodestake.top", "https://cosmos-lcd.quickapi.com:443"]
+SEMANTIC_VERSION_PATTERN = re.compile(r'v(\d+(?:\.\d+){0,2})')
+
+# Define all utility functions
+def download_and_extract_repo():
+    """Download the GitHub repository as a zip file, extract it, and return the path to the extracted content."""
+    global repo_path
+
+    # GitHub API endpoint to get the zip download URL
+    repo_api_url = "https://api.github.com/repos/cosmos/chain-registry"    
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+    }
+
+    print(f"Fetching repo {repo_api_url}...")
+    response = requests.get(repo_api_url, headers=headers)
+    
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch data from GitHub API. Status code: {response.status_code}")
+
+    response_data = response.json()
+    zip_url = response_data.get('archive_url', '').replace('{archive_format}', 'zipball').replace('{/ref}', '/master')
+
+    if not zip_url:
+        raise Exception("Failed to obtain the zip URL from the GitHub API response.")
+
+    # Download the zip file
+    zip_response = requests.get(zip_url, stream=True, headers=headers)
+    zip_filename = "chain-registry.zip"
+    with open(zip_filename, 'wb') as zip_file:
+        for chunk in zip_response.iter_content(chunk_size=8192):
+            zip_file.write(chunk)
+
+    # Extract the zip file
+    with zipfile.ZipFile(zip_filename, 'r') as zip_ref:
+        zip_ref.extractall('.')
+        # The extracted folder has a name like 'cosmos-chain-registry-<commit_hash>'
+        # We'll return the first directory that starts with 'cosmos-chain-registry-'
+        extracted_folder = next((folder for folder in os.listdir('.') if folder.startswith('cosmos-chain-registry-')), None)
+        repo_path = extracted_folder
+        return repo_path
 
 def fetch_directory_names(path):
     headers = {'Accept': 'application/vnd.github.v3+json'}
@@ -47,71 +91,49 @@ def fetch_directory_names(path):
     else:
         return []
 
-def fetch_all_chains():
-    # Fetch mainnet chains
-    mainnets = fetch_directory_names("")
-    if 'testnets' in mainnets:
-        mainnets.remove('testnets')  # Exclude the testnets directory
+def read_chain_json_from_local(network, base_path):
+    """Read the chain.json file for a given network from the local repository."""
+    chain_json_path = os.path.join(base_path, network, 'chain.json')
+    with open(chain_json_path, 'r') as f:
+        return json.load(f)
 
-    # Fetch testnet chains
-    testnets = fetch_directory_names("testnets")
-    print(f"mainnets: {mainnets}")
-    print(f"testnets: {testnets}")
-    return mainnets, testnets
+def process_data_for_network(network, full_data, network_type):
+    """Process the data for a specific network."""
+    network_data = full_data.get(network, [])
+    file_names = [item['name'] for item in network_data if item['type'] == 'file']
 
-def update_data():
-    """Function to update the data for mainnets and testnets every 5 minutes."""
-    global MAINNET_DATA, TESTNET_DATA
+    return {
+        "type": network_type,
+        "network": network,
+        "files": file_names
+    }
 
-    # Fetch the list of mainnets and testnets once
-    mainnets, testnets = fetch_all_chains()
+def process_data_for_local_network(network, base_path, network_type):
+    """Process the data for a specific network from local files."""
+    network_path = os.path.join(base_path, network)
+    file_names = [f for f in os.listdir(network_path) if os.path.isfile(os.path.join(network_path, f))]
 
-    while True:
-        try:
-            # Fetch data for mainnets
-            MAINNET_DATA = []
-            for network in mainnets:
-                try:
-                    network_data = fetch_data_for_network(network, MAINNET_BASE_URL, "mainnet")
-                    MAINNET_DATA.append(network_data)
-                except Exception as e:
-                    logging.error(f"Error fetching data for mainnet {network}: {e}")
-
-            # Fetch data for testnets
-            TESTNET_DATA = []
-            for network in testnets:
-                try:
-                    network_data = fetch_data_for_network(network, TESTNET_BASE_URL, "testnet")
-                    TESTNET_DATA.append(network_data)
-                except Exception as e:
-                    logging.error(f"Error fetching data for testnet {network}: {e}")
-
-            # Sleep for 5 minutes before the next update
-            sleep(300)
-        except Exception as e:
-            logging.error(f"Error in update_data loop: {e}")
-            sleep(60)  # If there's an error, wait for a minute before retrying
-
-def start_update_data_thread():
-    update_thread = threading.Thread(target=update_data)
-    update_thread.daemon = True 
-    update_thread.start()
+    return {
+        "type": network_type,
+        "network": network,
+        "files": file_names
+    }
 
 def get_healthy_rpc_endpoints(rpc_endpoints):
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
         healthy_rpc_endpoints = [rpc for rpc, is_healthy in executor.map(lambda rpc: (rpc, is_endpoint_healthy(rpc['address'])), rpc_endpoints) if is_healthy]
 
     return healthy_rpc_endpoints[:5]  # Select the first 5 healthy RPC endpoints
 
 def get_healthy_rest_endpoints(rest_endpoints):
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
         healthy_rest_endpoints = [rest for rest, is_healthy in executor.map(lambda rest: (rest, is_endpoint_healthy(rest['address'])), rest_endpoints) if is_healthy]
 
     return healthy_rest_endpoints[:5]  # Select the first 5 healthy REST endpoints
 
 def is_endpoint_healthy(endpoint):
     try:
-        response = requests.get(f"{endpoint}/health", timeout=3, verify=False)
+        response = requests.get(f"{endpoint}/health", timeout=1, verify=False)
         return response.status_code == 200
     except:
         return False
@@ -123,7 +145,7 @@ def get_healthy_endpoints(endpoints):
         if is_endpoint_healthy(endpoint['address']):
             healthy_endpoints.append(endpoint)
 
-    with ThreadPoolExecutor(max_workers=10) as executor:
+    with ThreadPoolExecutor(max_workers=num_workers) as executor:
         executor.map(check_endpoint, endpoints)
 
     return healthy_endpoints
@@ -249,12 +271,30 @@ def fetch_current_upgrade_plan(rest_url):
         print(f"Unhandled error while requesting current upgrade endpoint from {rest_url}: {e}")
         raise e
 
-def fetch_data_for_network(network, endpoints, network_type):
+def fetch_data_for_network(network, network_type):
+    global repo_path
+    """Fetch data for a given network."""
 
-    print(f"Fetching data for network {network}")
-    rest_endpoints = endpoints.get("rest", [])
-    rpc_endpoints = endpoints.get("rpc", [])
-    print(f"Found {len(rest_endpoints)} rest endpoints and {len(rpc_endpoints)} rpc endpoints")
+    # Construct the path to the chain.json file based on network type
+    if network_type == "mainnet":
+        chain_json_path = os.path.join(repo_path, network, 'chain.json')
+    elif network_type == "testnet":
+        chain_json_path = os.path.join(repo_path, 'testnets', network, 'chain.json')
+    else:
+        raise ValueError(f"Invalid network type: {network_type}")
+
+    # Check if the chain.json file exists
+    if not os.path.exists(chain_json_path):
+        print(f"chain.json not found for network {network}. Skipping...")
+        return None
+
+    # Load the chain.json data
+    with open(chain_json_path, 'r') as file:
+        data = json.load(file)
+
+    rest_endpoints = data.get("apis", {}).get("rest", [])
+    rpc_endpoints = data.get("apis", {}).get("rpc", [])
+    print(f"Found {len(rest_endpoints)} rest endpoints and {len(rpc_endpoints)} rpc endpoints for {network}")
 
     # Prioritize RPC endpoints for fetching the latest block height
     # Shuffle RPC endpoints to avoid calling the same one over and over
@@ -318,7 +358,50 @@ def fetch_data_for_network(network, endpoints, network_type):
         "rpc_server": rpc_server_used,
         "source": source
     }
+    print(f"Completed fetch data for network {network}")
     return output_data
+
+# periodic cache update
+def update_data():
+    """Function to update the data for mainnets and testnets every 5 minutes."""
+    while True:
+        print("Starting data update cycle...")
+        try:
+            repo_path = download_and_extract_repo()
+            print(f"Repository downloaded and extracted to: {repo_path}")
+
+            # Process mainnets & testnets
+            mainnet_networks = [d for d in os.listdir(repo_path) 
+                                if os.path.isdir(os.path.join(repo_path, d)) 
+                                and not d.startswith(('.', '_')) 
+                                and d != "testnets"]
+
+            testnet_path = os.path.join(repo_path, 'testnets')
+            testnet_networks = [d for d in os.listdir(testnet_path) 
+                                if os.path.isdir(os.path.join(testnet_path, d)) 
+                                and not d.startswith(('.', '_'))]
+
+            with ThreadPoolExecutor() as executor:
+                testnet_data = list(filter(None, executor.map(lambda network: fetch_data_for_network(network, "testnet"), testnet_networks)))
+                mainnet_data = list(filter(None, executor.map(lambda network: fetch_data_for_network(network, "mainnet"), mainnet_networks)))
+
+            # Update the Flask cache
+            cache.set('MAINNET_DATA', mainnet_data)
+            cache.set('TESTNET_DATA', testnet_data)
+
+            print("Data update cycle completed. Sleeping for 5 minutes...")
+            sleep(300)
+        except Exception as e:
+            print(f"Error in update_data loop: {e}")
+            print("Error encountered. Sleeping for 1 minute before retrying...")
+            sleep(60)
+
+def start_update_data_thread():
+    print("Starting the update_data thread...")
+    update_thread = threading.Thread(target=update_data)
+    update_thread.daemon = True 
+    update_thread.start()
+    print("update_data thread started.")
 
 @app.route('/healthz')
 def health_check():
@@ -332,12 +415,11 @@ def fetch_network_data():
             return jsonify({"error": "Invalid payload"}), 400
 
         results = []
-        for network_type, base_url, networks in [("mainnet", BASE_URL, request_data.get("MAINNETS", [])),
-                                                 ("testnet", BASE_URL + "/testnets", request_data.get("TESTNETS", []))]:
-            endpoints_map = fetch_all_endpoints(network_type, base_url, request_data)
+        for network_type, networks in [("mainnet", request_data.get("MAINNETS", [])),
+                                      ("testnet", request_data.get("TESTNETS", []))]:
             for network in networks:
                 try:
-                    network_data = fetch_data_for_network(network, endpoints_map.get(network, {}), network_type)
+                    network_data = fetch_data_for_network(network, network_type)
                     results.append(network_data)
                 except Exception as e:
                     logging.error(f"Error fetching data for network {network}: {e}")
@@ -350,14 +432,17 @@ def fetch_network_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/mainnet')
+@app.route('/mainnets')
+@cache.cached(timeout=300)  # Cache the result for 5 minutes
 def get_mainnet_data():
-    return jsonify(MAINNET_DATA)
+    return jsonify(cache.get('MAINNET_DATA'))
 
-@app.route('/testnet')
+@app.route('/testnets')
+@cache.cached(timeout=300)  # Cache the result for 5 minutes
 def get_testnet_data():
-    return jsonify(TESTNET_DATA)
+    return jsonify(cache.get('TESTNET_DATA'))
 
 if __name__ == '__main__':
+    app.debug = True
     start_update_data_thread()
     app.run(use_reloader=False)
